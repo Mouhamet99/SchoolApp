229b31792130da5026245cf7bf88ffa1a3c96b43
Mouhamet99 <dieyemouhamet1@gmail.com>
2021-12-25 16:58:44 +0000

    handle detail button to make detail modal content dynamic

--- src/apprenant.js
+++ src/apprenant.js
@@ -1,10 +1,8 @@
 import { getStudents } from './api.js'
 
 window.onload = function () {
+   let STUDENTS = []
    const cardContainer = document.querySelector('#students_container')
-
-
-
    const addCard = (student, index) => {
 
       const card = `
@@ -12,26 +10,22 @@ window.onload = function () {
             <div class="card bg-light p-3 m-2 stduents-card"  id="${student.id}">
                <div class="row">
                   <div class="d-flex flex-row align-items-center" >
-                     <!-- <div class="icon"> <i class="fa fa-twitter"></i> </div> -->
-                     <div class="icon"> <img class="profile-user-img rounded-circle img-fluid"
+                     
+                     <div class="icon border border-4 border-secondary rounded-circle d-flex"> 
+                     <img class="rounded-circle img-fluid"
                            src="${student['data']['image']['url']}" alt="student profile image"> </div>
                      <div class="ms-2 c-details">
                         <h6 class="mb-0" data-last-name="${student['data'].last_name}" data-first-name="${student['data'].first_name}" >${student['data'].first_name} ${student['data'].last_name}</h6>
-                        <strong>Debutant</strong>
+                        <strong class="lead fs-6">Debutant</strong>
                      </div>
-                     <div class="d-inline rounded p-1 ms-auto align-self-start">
-                        <a class="border-0 btn-transition btn btn-sm btn-outline-warning  edit_student" id="edit-student-${student.id}"
-                        title="editer etudiant"><i class="fa fa-edit"></i>
-                        </a>
-                        <a class="border-0 btn-transition btn btn-sm btn-outline-danger remove_student" id="remove-student-${student.id}"
-                           title="supprimer etudiant"><i class="fa fa-trash"></i>
-                        </a>
-                     </div>
+                     
+                     <button id="detail" class="btn-detail ms-auto align-self-start rounded btn btn-outline-secondary btn-sm px-2 py-0" data-card-detail="${student.id}">Detail</button>
+
                   </div>
                </div>
                <div class="row">
                   <div class="mt-3">
-                     <strong class="d-block">Biographie</strong>
+                     <strong class="d-block lead ">Biographie</strong>
                      <span class="text-muted">${student['data'].bio}</span>
                   </div>
                </div>
@@ -41,28 +35,54 @@ window.onload = function () {
 
       cardContainer.insertAdjacentHTML('beforeend', card)
 
-      // document.getElementById(`edit-student-${student.id}`).addEventListener('click', () => {
+      const cardElement = document.querySelector(`.card[id="${student.id}"]`)
+      cardElement.querySelector(`.btn-detail`).addEventListener('click', (e) => {
+         e.preventDefault()
+         updateDetailModal(student.data)
 
-      //    lastName.value = student["data"].last_name
-      //    firstName.value = student["data"].first_name
-      //    bio.value = student["data"].bio
-      //    level.value = student["data"].level
-      //    file.files[0] = { ...student["data"].image }
-      //    // levelOption.selected = true
-
-      //    btnFormSubmit.classList.add('d-none')
-      //    btnFormEdit.classList.remove('d-none')
-      //    btnFormEdit.setAttribute('data-card-to-update', student.id)
-      // })
-      // document.getElementById(`remove-student-${student.id}`).addEventListener('click', () => {
-      //    removeStudent(student.id, index)
-      // })
+      })
    }
    getStudents().then((students) => {
+      STUDENTS = students;
       students.forEach((student, index) => {
          addCard(student, index)
-      });
-   });
 
+      });
+      console.log(STUDENTS);
+   });
+   const updateDetailModal = (student) => {
+      const modal = document.querySelector('#detail-modal')
+      const modalTrigger = document.querySelector('[data-bs-target="#detail-modal"]')
+      const profile = modal.querySelector('#profil')
+      const username = modal.querySelector('#username')
+      const level = modal.querySelector('#level')
+      const bio = modal.querySelector('#bio')
+      const skills = modal.querySelector('#skills')
+      const colors = ["primary", "warning", "danger", "info","success"]
+
+      profile.src = student['image']['url']
+      username.textContent = student['first_name'] + ' ' + student['last_name']
+      level.textContent = student['level']
+      bio.textContent = student['bio']
+      skills.innerHTML = ''
+
+      student['skills'].forEach((skill, i) => {
+         skill = JSON.parse(skill)
+
+         let [skillLabel] = Object.keys(skill)
+         let [skillProgressValue] = Object.values(skill)
+         const skillItemElement = `
+            <div class="row py-3 skill-item">
+               <label class="px-0 " id="skill-item-label">${skillLabel}</label>
+               <div class="progress px-0">
+                  <div id="progress-bar"class="progress-bar bg-${colors[i]}"  role="progressbar" style="width: ${skillProgressValue}%;"
+                     aria-valuenow="${skillProgressValue}" aria-valuemin="0" aria-valuemax="100">${skillProgressValue}%</div>
+               </div>
+            </div>
+         `
+         skills.insertAdjacentHTML('beforeend', skillItemElement)
+      })
+      modalTrigger.click()
+   }
 
 }
